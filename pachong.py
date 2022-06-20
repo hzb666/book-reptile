ac07ab748ed4e23d6cd316b376a55b3ba3d06a90
huzhibin <15224214063@163.com>
2022-06-20 22:49:06 +0800

    Update pachong.py

--- pachong.py
+++ pachong.py
@@ -8,7 +8,6 @@ import os
 # 此字符串为参数url，页码尾数为tail_id, 保存文件夹目录为dirpath
 # 参数输入到函数szsy_get中即可
 
-# 有机化学
 url = 'http://162.105.138.126/OnLineReader/command/imagepage.ashx?objID=obj201409267qul6pld1gs390nn0000058167&metaId=meta201407027qul6pld00000520869&OrgId=apabi_usp&Ip=undefined&scale=1.7611447113873502&width=5054&height=7240&pageid={}&ServiceType=Imagepage&scaleType=1&OrWidth=663.999883910117&OrHeight=951.999888201652&testres=&debug=&SessionId=B61D3955674FFB04&UserName=%E8%83%A1%E5%BF%97%E6%96%8C&cult=CN&rights=1-0_00&time=2022-06-20%2014:59:43&sign=410F5631A58BC4E036FAB2B2DA607A3F'
 
 tail_id = 6
